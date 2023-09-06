190b29abd8fe8e99578bb05026daf4cd77410660
gavrielJaffe <gavrielmn@gmail.com>
2023-09-06 18:12:44 +0300

    script to validate if a number is Palindrome or not

---
+++ isPalindrome.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        # Test for negative numbers
+        if x < 0 :
+            return False
+        x = str(x)
+        half_size = len(x) / 2 
+        half_size = int(half_size)
+        # Test for evens
+        if len(x) % 2 == 0 : 
+            for i in range((half_size)):
+                other_three = len(x)-1 - i 
+                if x[i] == x[other_three]:
+                    continue
+                else:
+                    return False
+        # Test for odds
+        elif (len(x) % 2) == 1 :
+            for i in range((half_size)):
+                other_three = len(x)-1 - i 
+                if x[i] == x[other_three]:
+                    print(f'comeper {x[i]}-{x[other_three]}')
+                    continue
+                else:
+                    return False
+        return True
+
+x = 12421
+
+sol = Solution()
+answer = sol.isPalindrome(x)
+print('answer',answer)
