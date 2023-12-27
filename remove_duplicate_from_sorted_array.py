e54a648eb8f0f69405dab49ec3a0a77eff0788ce
gavrielJaffe <gavrielmn@gmail.com>
2023-12-27 18:39:53 +0200

    test if k is right

--- remove_duplicate_from_sorted_array.py
+++ remove_duplicate_from_sorted_array.py
@@ -1,29 +1,41 @@
 from typing import List
+
 nums = [0,0,1,1,1,2,2,3,3,4]
+# nums = [1,1,2]
 
 class Solution:
-
     @staticmethod
-    def move_to_left(nums):
-        
+    def move_to_left(nums,flag):
+        """
+        Move every element that is duplicate from the start until the end.
+        append "_" in the end of the list,
+        end of the list would be the first '_'.
+
+        """
+
         for i in range(1,len(nums)):
-            if (nums[i] == nums[i-1]) and not(nums[i] == '_'):
-                # print(f'{nums[i]} == {nums[i-1]}')
+            if nums[i] == nums[i-1]:
                 element = nums.pop(i)
                 nums[i-1] = element
                 nums.append('_')
                 break
-        return nums 
+            # We rised a flag,Because we got to the end the first _
+            if (nums[i] == '_'):
+                flag = True # or False
+                break
+        return nums,flag ,i
     
     def removeDuplicates(self, nums: List[int]) -> int:
+        flag = False
         for i in range(1,len(nums)):
-            # Move to every element to the left by 1,all of them.
-            nums = self.move_to_left(nums)
+            nums,flag ,k = self.move_to_left(nums,flag)
             
-        return nums
-       
+            if flag == True:
+                print("break becuase we raised a flag", flag)
+                break
+        return k, nums
     
 sol  = Solution()
-ans= sol.removeDuplicates(nums)
-print('', )
+k, ans = sol.removeDuplicates(nums)
 print('ans', ans)
+print('k', type(k))
