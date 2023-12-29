6497500ff4d878c1e9275a3c8fac65d238b2c673
gavrielJaffe <gavrielmn@gmail.com>
2023-12-29 16:05:07 +0200

    add the select_data function

--- select_data.py
+++ select_data.py
@@ -1,7 +1,23 @@
 import pandas as pd 
 from typing import List
 """
-
+2880. Select Data 
+Example 1:
+Input:
++------------+---------+-----+
+| student_id | name    | age |
++------------+---------+-----+
+| 101        | Ulysses | 13  |
+| 53         | William | 10  |
+| 128        | Henry   | 6   |
+| 3          | Henry   | 11  |
++------------+---------+-----+
+Output:
++---------+-----+
+| name    | age | 
++---------+-----+
+| Ulysses | 13  |
++---------+-----+
 
 """
 data = [
@@ -10,13 +26,10 @@ data = [
     {'student_id': 128, 'name': 'Henry', 'age': 6},
     {'student_id': 3, 'name': 'Henry', 'age': 11}
 ]
-
-
 students = pd.DataFrame(data)
-
+# 
 def selectData(students: pd.DataFrame) -> pd.DataFrame:
     columns_name = ["name","age"]
-    df = students.loc[students['student_id'] == 101][columns_name]
-    return df 
+    return students.loc[students['student_id'] == 101][columns_name]
 
 print(selectData(students))
