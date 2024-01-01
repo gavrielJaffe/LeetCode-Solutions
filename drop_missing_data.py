c9c0b6c42e4b4344ad9713999d55c6cdb7890481
gavrielJaffe <gavrielmn@gmail.com>
2024-01-01 17:36:46 +0200

    drop missing data.py file is well , best way to remove data that is null is by a build in function

--- drop_missing_data.py
+++ drop_missing_data.py
@@ -2,10 +2,14 @@ import pandas as pd
 
 data = [
     {"student_id": 572, "name": "Henry", "age": 8},
+    {"student_id": 564, "name": None, "age": 13} , 
     {"student_id": 271, "name": None, "age": 17},
     {"student_id": 315, "name": "Liam", "age": 7},
     {"student_id": 615, "name": "Charlie", "age": 9},
-    {"student_id": 564, "name": None, "age": 13}
+    {"student_id": 514, "name": None, "age": 33} , 
+    {"student_id": 568, "name": None, "age": 23} , 
+    {"student_id": 314, "name": None, "age": 15},
+    {"student_id": 981, "name": None, "age": 23},
 ]
 
 """
@@ -42,30 +46,28 @@ def createDataframe(data):
     df = pd.DataFrame(data,columns = ["student_id","name","age"])
     return df
 
-# def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
-#     y = 0
-#     for i in range(students.shape[1]+1):
-#         a = students["name"][i] == None # True
-#         if (a):
-#             y = i
-#     students = students.drop(index=y)
-#     return students 
-
-
+# Beats 69.60 % of users with Pandas
 def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
-    y = 0
-    size = students.shape[1]+1
-    print('********************************************************')
-    for i in range(size):
-        print('i', i)
-        a = students["name"][i] == None  
-        if (a):
-            y = i
-            students = students.drop(index=y)
-
+    size = students.shape[0]
+    i = 0
+    while (i < size):
+        if (students["name"][i] == None ):
+            students = students.drop(index=i)
+        i+=1
     return students
 
+# Beats 63.75 % of users with Pandas
+def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
+    students.dropna(subset=['name'], inplace=True)
+    return students
 
+# Beats 79.51 % of users with Pandas
+def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
+    return pd.DataFrame(students)[students['name'].notnull()]
+
+#Best way - Beats 86.48 % of users with Pandas
+def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
+    return pd.DataFrame(students)[students['name'].notna()]
 
 df = createDataframe(data)
 # print(df)
