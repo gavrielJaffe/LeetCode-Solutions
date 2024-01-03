a048272d2a41ae4e04910abc27dc7832a30e62c7
gavrielJaffe <gavrielmn@gmail.com>
2024-01-03 17:40:46 +0200

    works local, need in leetcode

--- employees_earning_more_than_their_managers.py
+++ employees_earning_more_than_their_managers.py
@@ -67,42 +67,67 @@ data1 = [
     }
 ]
 
-df = pd.DataFrame(data1)
+df1 = pd.DataFrame(data)
 
-# works for data ,not for data1 
-def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
-    d = {'Employee': []}
-    df4 = pd.DataFrame(data=d)   
-    for i in range(employee.shape[0]): # bug here
-        if (isinstance(employee['managerId'].iloc[i], int)):
-            pos = (employee['managerId'].iloc[i])
-            if ((employee['salary'].iloc[i]) > (employee.loc[employee['id'][pos],'salary'])):
-                username = employee['name'][i] # Mark ,Jack
-                # Apeend to df4 
-                new_row = {'Employee': username}
-                df4.loc[len(df4)] = new_row
-    return df4
+# works for data1 ,not for data 
+# def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
+#     # Build a new df 
+#     df4 = pd.DataFrame(data={'Employee': []})   
 
-# with gpt
+#     for i in range(employee.shape[0]): 
+#         manager_id = employee['managerId'].iloc[i]
+#         if (isinstance(manager_id, int)): # test for int only , not null
+#             salary_of_employee = employee['salary'].iloc[i]
+#         if ([employee["id"] == manager_id]):
+#             salary_of_manager = employee.loc[employee['id'] == manager_id, 'salary'].values
+        
+#         if salary_of_manager.size > 0:
+
+#             if ((salary_of_employee) > (salary_of_manager)):
+#                 username = employee['name'][i] # Mark ,Jack
+#                 # Apeend to df4 
+#                 new_row = {'Employee': username}
+#                 df4.loc[len(df4)] = new_row
+#     return df4
+
+
 def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
-    df4 = pd.DataFrame(columns=['Employee'])  # Create an empty DataFrame with column 'Employee'
-    
-    for i in range(employee.shape[0]):
+    # Build a new df 
+    df4 = pd.DataFrame(data={'Employee': []})   
+
+    for i in range(employee.shape[0]): 
         manager_id = employee['managerId'].iloc[i]
-        if manager_id != 'null' and pd.notnull(manager_id):
-            manager_id = int(manager_id)
-            if employee['salary'].iloc[i] > employee.loc[employee['id'] == manager_id, 'salary'].values:
-                username = employee['name'].iloc[i]  # Get the employee's name
-                df4 = df4._append({'Employee': username}, ignore_index=True)  # Append to df4
-    
+        if (isinstance(manager_id, int)): # test for int only , not null
+            salary_of_employee = employee['salary'].iloc[i]
+        if ([employee["id"] == manager_id]):
+            salary_of_manager = employee.loc[employee['id'] == manager_id, 'salary'].values
+        
+            if salary_of_manager.size > 0:
+
+                if ((salary_of_employee) > (salary_of_manager)):
+                    username = employee['name'][i] # Mark ,Jack
+                    # Apeend to df4 
+                    new_row = {'Employee': username}
+                    df4.loc[len(df4)] = new_row
+                
     return df4
 
 
-ans = find_employees(df)
+
+
+
+
+
+
+
+
+
+
+
+
+
+ans = find_employees(df1)
 print(ans)
-
-
-
 
 
 
@@ -134,3 +159,41 @@ print(ans)
     # wrk_df = wrk_df[wrk_df['salary'] > manger_sal_df['salary']]
     # rest = pd.DataFrame(wrk_df['name']).rename(columns={"name":"Employee"})
     # return rest
+
+
+
+# with gpt
+# def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
+#     df4 = pd.DataFrame(columns=['Employee'])  # Create an empty DataFrame with column 'Employee'
+    
+#     for i in range(employee.shape[0]):
+#         manager_id = employee['managerId'].iloc[i]
+#         if manager_id != 'null' and pd.notnull(manager_id):
+#             manager_id = int(manager_id)
+#             if employee['salary'].iloc[i] > employee.loc[employee['id'] == manager_id, 'salary'].values:
+#                 username = employee['name'].iloc[i]  # Get the employee's name
+#                 df4 = df4._append({'Employee': username}, ignore_index=True)  # Append to df4
+    
+#     return df4
+
+
+# last version
+# def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
+#     print(employee)
+#     # Build a new df 
+#     d = {'Employee': []}
+#     df4 = pd.DataFrame(data=d)   
+#     # go over the len of df 
+#     for i in range(employee.shape[0]): # bug here
+#         manager_id = employee['managerId'].iloc[i]
+#         if (isinstance(manager_id, int)): # test for int only , not null
+#             salary_of_employee = employee['salary'].iloc[i]
+#             # print('manager_id', manager_id)
+#         if ([employee["id"] == manager_id]):
+#             salary_of_manager = df.loc[df['id'] == manager_id, 'salary'].values
+#             if ((salary_of_employee) > (salary_of_manager)):
+#                 username = employee['name'][i] # Mark ,Jack
+#                 # Apeend to df4 
+#                 new_row = {'Employee': username}
+#                 df4.loc[len(df4)] = new_row
+#     return df4
