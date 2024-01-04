2995033b904cfcb7204802398d685375e8ecfa28
gavrielJaffe <gavrielmn@gmail.com>
2024-01-05 00:33:16 +0200

    some work on the file

--- second_highest_salary.py
+++ second_highest_salary.py
@@ -21,13 +21,20 @@ data4 = {
     'salary': [100, 100, 50]
 } # Outpot 50
 
-employee_df = pd.DataFrame(data4)
+data5 = {
+    'id': [1, 2, 3,4,5],
+    'salary': [100, 100, 50,30,20]
+    # 'salary': [100, 100, 50,30,20] -> [50,30,20]
+} # Outpot 50
+
+
+employee_df = pd.DataFrame(data5)
 print(employee_df)
 
 def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
     max_salary = employee['salary'].max()
     max_salary_index = employee['salary'].idxmax()
-    employee = employee.drop([max_salary_index])
+    employee = employee.drop([max_salary_index]) # not all of the use cases are falling in. data5 , data4
     employee = employee['salary'].max()
     if max_salary == employee: # in case of two employee in the same salary we would send null
         employee = None
@@ -35,13 +42,3 @@ def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
 
 ans = second_highest_salary(employee_df)
 print(ans)
-
-# def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
-#     max_salary = employee['salary'].max()
-#     max_salary_index = employee['salary'].idxmax()
-#     employee = employee.drop([max_salary_index])
-#     employee = employee['salary'].max()
-#     if max_salary == employee: # in case of two employee in the same salary we would send null
-#         employee = None
-#     df = pd.DataFrame({employee},columns=["SecondHighestSalary"])
-#     return df 
