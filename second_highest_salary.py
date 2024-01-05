33560575cbc7b24aa418cb06c1c5edfb741801c0
gavrielJaffe <gavrielmn@gmail.com>
2024-01-05 02:31:14 +0200

    find the second highest salary using pytong and pandas

--- second_highest_salary.py
+++ second_highest_salary.py
@@ -30,22 +30,20 @@ data5 = {
 
 employee_df = pd.DataFrame(data3)
 
+# Beats 89.99 % of users with Pandas
 def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
-    # find all of the indexes of the max_salary in the df.
+    # Find all of the indexes of the max_salary in the df.
     max_salary = employee['salary'].max()
-    # print(max_salary)
     indexs_of_max_salary = (employee[employee['salary'] == max_salary].index).tolist()
-    # drop of indexs_of_max_salary,saved to the df.delete all of the indexes of max_salary from the df.
+    # Drop rows with indices stored in indices_of_max_salary from the DataFrame
     employee = employee.drop(indexs_of_max_salary)
-    print(employee)
-    employee = employee['salary'].max()
-    if max_salary == employee: # in case of two employee in the same salary we would send null
-        employee = None
+    employee = employee['salary'].max() # second highest max
     return pd.DataFrame({employee},columns=["SecondHighestSalary"])
 
 ans = second_highest_salary(employee_df)
 print(ans)
 
+# Beats 70.97%of users with Pandas
 def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
     max_salary = employee['salary'].max()
     indexs_of_max_salary = (employee[employee['salary'] == max_salary].index).tolist()
