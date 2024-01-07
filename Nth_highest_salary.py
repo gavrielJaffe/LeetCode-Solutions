c558e8bbdd68bc92b7b114020d1def256310ad8f
gavrielJaffe <gavrielmn@gmail.com>
2024-01-07 15:12:31 +0200

    Nth_highest_salary.py

---
+++ Nth_highest_salary.py
@@ -0,0 +1,46 @@
+
+
+
+import pandas as pd
+
+data = {
+    'id': [1, 2, 3],
+    'salary': [100, 200, 300]
+} # Outpot 200
+
+data2 = {
+    'id': [1],
+    'salary': [100]
+} # Outpot None
+
+data3 = {
+    'id': [1,2,3,4],
+    'salary': [100, 100, 100,100]
+} # Outpot None
+
+data4 = {
+    'id': [1, 2, 3],
+    'salary': [100, 100, 50]
+} # Outpot 50
+
+data5 = {
+    'id': [1, 2, 3,4,5],
+    'salary': [100, 100, 50,30,20]
+    # 'salary': [100, 100, 50,30,20] -> [50,30,20]
+} # Outpot 50
+
+employee_df = pd.DataFrame(data)
+
+def nth_highest_salary(employee: pd.DataFrame , N :int) -> pd.DataFrame:
+    # Find all of the indexes of the max_salary in the df.
+    max_salary = employee['salary'].max()
+    indexs_of_max_salary = (employee[employee['salary'] == max_salary].index).tolist()
+    # Drop rows with indices stored in indices_of_max_salary from the DataFrame
+    employee = employee.drop(indexs_of_max_salary)
+    employee = employee['salary'].max() # second highest max
+        
+    return pd.DataFrame({employee},columns=["SecondHighestSalary"])
+
+ans = nth_highest_salary(employee_df,3)
+print('ans', ans)
+
