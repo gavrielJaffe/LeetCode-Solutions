1c7de5567126f847439f5efa2b7a65fb938253eb
gavrielJaffe <gavrielmn@gmail.com>
2024-02-19 20:45:29 +0200

    basic

--- employee_bonus.py
+++ employee_bonus.py
@@ -1,1 +1,24 @@
+import pandas as pd 
 
+data = {
+    'empId': [3, 1, 2, 4],
+    'name': ['Brad', 'John', 'Dan', 'Thomas'],
+    'supervisor': [None, 3, 3, 3],
+    'salary': [4000, 1000, 2000, 4000]
+}
+
+data_bonus = {
+    'empId': [2, 4],
+    'bonus': [500, 2000]
+}
+
+df_bonus = pd.DataFrame(data_bonus)
+df_emp = pd.DataFrame(data)
+# name and bonus amount of each employee with a bonus less than 1000
+
+
+def employee_bonus(employee: pd.DataFrame, bonus: pd.DataFrame) -> pd.DataFrame:
+    print(employee)
+    print(bonus)
+
+employee_bonus(df_emp ,df_bonus)
