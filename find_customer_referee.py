3c8cde499f4d47f288a611dd87c039a4a35dd7a6
gavrielJaffe <gavrielmn@gmail.com>
2024-02-20 22:03:46 +0200

    works over vscode need to test validation ind find_customer over leetcode

--- find_customer_referee.py
+++ find_customer_referee.py
@@ -1,4 +1,17 @@
 import pandas as pd
 
+data = {
+    'id': [1, 2, 3, 4, 5, 6],
+    'name': ['Will', 'Jane', 'Alex', 'Bill', 'Zack', 'Mark'],
+    'referee_id': [None, None, 2, None, 1, 2]
+}
+df = pd.DataFrame(data)
+
 def find_customer_referee(customer: pd.DataFrame) -> pd.DataFrame:
-    pass
+    mask = customer['referee_id'] == 2
+    customer = customer[~mask]
+    df = customer[['name']]
+    return df 
+
+ans = find_customer_referee(customer=df)
+print(ans)
