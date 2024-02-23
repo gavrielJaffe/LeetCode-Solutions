3e73916fe1a427f4b6963fc9a4089f51474f7a64
gavrielJaffe <gavrielmn@gmail.com>
2024-02-23 16:40:36 +0200

    convert to a df

--- big_countries.py
+++ big_countries.py
@@ -1,15 +1,18 @@
 import pandas as pd
 
-world_data = {
+data = {
     "name": ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola"],
     "continent": ["Asia", "Europe", "Africa", "Europe", "Africa"],
     "area": [652230, 28748, 2381741, 468, 1246700],
     "population": [25500100, 2831741, 37100000, 78115, 20609294],
     "gdp": [20343000000, 12960000000, 188681000000, 3712000000, 100990000000]
 }
-df = pd.DataFrame(world_data)
+
+
+
+df = pd.DataFrame(data)
 
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
-    pass
+    print(world)
 
 big_countries(df)
