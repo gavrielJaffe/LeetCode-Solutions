b9ebfd8d2942706325a2ca1510847c8d9b365eb0
gavrielJaffe <gavrielmn@gmail.com>
2024-02-25 17:54:46 +0200

    save

--- big_countries.py
+++ big_countries.py
@@ -8,7 +8,7 @@ data = {
     "gdp": [20343000000, 12960000000, 188681000000, 3712000000, 100990000000]
 }
 
- # need to be in 3 cratieria 
+ # need to be in 3 cratieria  
 
 df = pd.DataFrame(data)
 
