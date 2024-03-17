4ee05c06ba1090f9c0163d026768579691022fb5
gavrielJaffe <gavrielmn@gmail.com>
2024-03-17 21:35:06 +0200

    big countries

--- big_countries.py
+++ big_countries.py
@@ -8,7 +8,6 @@ data = {
     "gdp": [20343000000, 12960000000, 188681000000, 3712000000, 100990000000]
 }
  # need to be in 3 cratieria  
-
 df = pd.DataFrame(data)
 
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
